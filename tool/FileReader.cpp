aed1de798b7f3dc3cdeff47ce05b4d47512953e1
GabrielCambe <almeidasalesgabriel@gmail.com>
2022-05-11 17:34:21 -0300

    novas modificações

---
+++ tool/FileReader.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+class FileReader {
+    private:
+        std::string filename;
+    public:
+        std::string current_line;
+        void open(std::string filename);
+        void readline();
+        void close();
+};
+
+void FileReader::open(std::string filename){
+
+}
+
+void FileReader::readline(){
+
+}
+
+void FileReader::close(){
+
+}
+
+int main(int argc, char **argv) {
+    FileReader file;
+    file.open("teste.txt");
+    std::cout << file.current_line << std::endl;
+    file.readline();
+    std::cout << file.current_line << std::endl;
+}
